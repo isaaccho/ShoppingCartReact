ca80abfc5f1458b937376785e5bebf347c17e42d
isaaccho <isaacshcho@gmail.com>
2019-06-04 15:14:45 -0400

    updates of classname and style

--- shopping-cart/src/components/counter.jsx
+++ shopping-cart/src/components/counter.jsx
@@ -2,16 +2,27 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1 + 1
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold"
   };
 
   render() {
     return (
       //every render can only return one tag (ex. everything has to be inside <div> </div>)
       <div>
-        <span>{this.formatCount()}</span>
+        <img src={this.state.imageUrl} alt="" />
+        <span style={this.styles} className="badge badge-warning m-2">
+          {this.formatCount()}
+        </span>
 
-        <button>Increment</button>
+        <button style={{ fontSize: 100 }} className="btn btn-secondary btn-sm">
+          Increment
+        </button>
       </div>
     );
   }
